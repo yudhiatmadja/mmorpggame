21043e069c03c4b46d8a9df23f403dc90881125b
yudhiiatmadja <yudhi.gamedev@gmail.com>
2025-07-29 11:04:22 +0700

    ganti skin character

---
+++ Assets/AllStarCharacterLibrary/Scripts/killParticle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class KillParticle : MonoBehaviour 
+{
+    private ParticleSystem ps;
+    public float lifespan;
+    private float startTime;
+
+    void Start () 
+    {
+        ps = GetComponent<ParticleSystem>();
+        startTime = Time.time;
+    }
+    
+    void Update () 
+    {
+        if ((Time.time - startTime) > lifespan || (ps && !ps.IsAlive()))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
